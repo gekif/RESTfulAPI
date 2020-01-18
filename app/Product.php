ef9f2786bf2dc02f8613c4cc03bdffe44dd09d20
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-18 22:08:58 +0700

    8. Implementing the RESTful API Models and its Relationships using Laravel Eloquent: 1. Implementing The Properties for Category 2. Implementing The Properties for Product 3. Implementing The Properties for Transaction 4. Implementing The Properties for User

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    const AVAIABLE_PRODUCT = 'available';
+    const UNAVAIABLE_PRODUCT = 'unavailable';
+
+
+    protected $fillable = [
+        'name',
+        'description',
+        'quantity',
+        'status',
+        'image',
+        'seller_id'
+    ];
+
+
+    public function isAvailable()
+    {
+        return $this->status == Product::AVAIABLE_PRODUCT;
+    }
 }
