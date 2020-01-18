aaba79c9c812a55ddec4c41471d3e1d6aa3255d1
Dzulfikar Maulana <dzulfikar.maulana@brainworx.co.id>
2020-01-18 22:27:47 +0700

    9. Creating the Database Structure Using Migrations from Laravel: 1. Solving a Common Issue with the Laravel Migrations 2. Implementing The Migration for Users 3. Implementing The Migration for Categories 4. Implementing The Migration for Products 5. Implementing The Migration for Transactions 6. Creating the Migration for The Pivot Table

--- database/migrations/2020_01_18_112841_create_products_table.php
+++ database/migrations/2020_01_18_112841_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Product;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -15,7 +16,15 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->string('decription', 1000);
+            $table->integer('quantity')->unsigned();
+            $table->string('status')->default(Product::UNAVAIABLE_PRODUCT);
+            $table->string('image');
+            $table->integer('seller_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('selled_id')->references('id')->on('users');
         });
     }
 
