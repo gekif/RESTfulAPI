d528a99d9326445c87ad471760266e1a2e059cbd
Gekif <dzulfikar.maulana@gmail.com>
2020-03-01 09:44:23 +0700

    29. Transforming Responses with PHP Fractal for Security and Compatibility: 3. Creating the Transformer for User 4. Creating the Transformer for Seller and Buyer 5. Creating the Transformer for Category 6. Creating the Transformer for Transaction 7. Creating the Transformer for Product 8. Linking the Models with its Respective Transformer 9. Returning the New Transformed Responses

--- app/Product.php
+++ app/Product.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Transformers\ProductTransformer;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -12,6 +13,9 @@ class Product extends Model
 
     const AVAIABLE_PRODUCT = 'available';
     const UNAVAIABLE_PRODUCT = 'unavailable';
+
+
+    public $transformer = ProductTransformer::class;
 
 
     protected $dates = ['deleted_at'];
