c9246c69dfa6d52abe499dc518a8d5839fe1d6fb
Gekif <dzulfikar.maulana@gmail.com>
2020-03-02 21:07:50 +0700

    39. CORS for Laravel: 2. Installing the CORS Package for Laravel 3. Configuring the CORS Package 4. Allowing CORS Only for the API 5. Allowing CORS on Error Responses Too

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use App\Traits\ApiResponser;
+use Barryvdh\Cors\CorsService;
 use Exception;
 use HttpException;
 use Illuminate\Auth\Access\AuthorizationException;
@@ -57,6 +58,16 @@ class Handler extends ExceptionHandler
      * @return \Illuminate\Http\Response
      */
     public function render($request, Exception $exception)
+    {
+        $response = $this->handleException($request, $exception);
+
+        app(CorsService::class)->addActualRequestHeaders($response, $request);
+
+        return $response;
+    }
+
+
+    public function handleException($request, Exception $exception)
     {
         if ($exception instanceof ValidationException) {
             return $this->convertValidationExceptionToResponse($exception, $request);
